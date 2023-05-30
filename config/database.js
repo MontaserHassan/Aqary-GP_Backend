e650800215a20f469f5123a065dbe62351a5af19
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-30 09:15:28 +0300

    add logging system

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const mongoClient = require('mongoose');
+const logger = require('./logger');
 require('dotenv').config();
 const { MONGO_URL } = process.env;
 
 mongoClient.connect(MONGO_URL)
-    .then(() => console.log('Connected to database'))
+    .then(() => logger.info('Connected to database'))
     .catch(error => console.log('Error connecting to database', error));
 
 module.exports = mongoClient;
