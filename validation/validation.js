f15cca70df7526c16666f18473c71d26afa01ce5
MontaserHassan <montaser.hassan.bns@gmail.com>
2023-06-02 20:49:13 +0300

    modify in validation file

--- validation/validation.js
+++ validation/validation.js
@@ -1,35 +1,30 @@
 const Joi = require("joi");
-const asyncFunction = require('../middlewares/async');
+const asyncFunction = require("../middlewares/async");
 
-const validation = (schema) => asyncFunction(async (req, res, next) => {
-    const errorValidation = [];
-    ['params', 'query','body'].forEach((key) => {
-        if (schema[key]) {
-            const validation = schema[key].validate(req[key]);
-            if (validation.error) {
-                errorValidation.push(validation.error);
+const validation = (schema) =>
+    asyncFunction(async (req, res, next) => {
+        const errorValidation = [];
+        ["params", "query", "body"].forEach((key) => {
+            if (schema[key]) {
+                const validation = schema[key].validate(req[key]);
+                if (validation.error) {
+                    errorValidation.push(validation.error);
+                }
             }
+        });
+        if (errorValidation.length > 0) {
+            throw { status: 422, message: errorValidation[0].details[0].message };
+        } else {
+            next();
         }
     });
-    if (errorValidation.length > 0) {
-        throw {status: 422, message: errorValidation[0].details[0].message};
-    } else {
-        next();
-    }
-});
-
 
 const propertyValidator = {
-    createProperty: {
-
-    },
-    updateProperty: {
-
-    }
-}
-
-
+    createProperty: {},
+    updateProperty: {},
+};
 
 module.exports = {
     validation,
-}
+    propertyValidator,
+};
