7f87c082ed4409e88516ddf58d731b5c5148ecaf
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2023-06-03 01:58:30 +0300

    update property

--- controllers/property.js
+++ controllers/property.js
@@ -27,8 +27,7 @@ const addProperty = (req, res, next) => {
     description: description,
     price: price,
     contractPhone: contractPhone,
-    photos,
-    photos,
+    photos: photos,
     paymentOption: paymentOption,
     subscribe: subscribe,
   });
@@ -54,13 +53,54 @@ const getAllProperties = (req, res, next) => {
 };
 
 const getProperty = (req, res, next) => {
-  const { params: { id } } = req;
+  const {
+    params: { id },
+  } = req;
 
   Property.findById(id)
     .then((response) => {
       res.json(response);
     })
-    .catch((error) => { res.json(error) });
+    .catch((error) => {
+      res.json(error);
+    });
 };
 
-module.exports = { addProperty, getAllProperties, getProperty };
+// edit Property by id  
+const editProperty = (req, res, next) => {
+  const { params: { id } } = req;
+  const {
+    address: address,
+    city: city,
+    title: title,
+    level: level,
+    rooms: rooms,
+    baths: baths,
+    area: area,
+    description: description,
+    price: price,
+    contractPhone: contractPhone,
+    photos: photos,
+    paymentOption: paymentOption,
+    subscribe: subscribe,
+  } = req.body;
+  Property.findById(id).then((property) => {
+    property.address = address;
+    property.city = city;
+    property.level = level;
+    property.rooms = rooms;
+    property.baths = baths;
+    property.area = area;
+    property.description = description;
+    property.contractPhone = contractPhone;
+    property.photos = photos;
+    property.paymentOption = paymentOption;
+    property.subscribe = subscribe;
+
+   return property.save();
+  })
+.then(()=>{res.json("Updated Succesful")})
+.catch((error)=>{res.json(error)});
+};
+
+module.exports = { addProperty, getAllProperties, getProperty, editProperty };
--- routes/backOffice/backOffice.js
+++ routes/backOffice/backOffice.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 
-const propertyController = require('../../controllers/property');
+const propertyController = require("../../controllers/property");
 
 const router = express.Router();
 
-
 // middleware for check is admin or not
 
+// all admin routes
 
-// all admin routes 
-
-router.post('/property', propertyController.addProperty);
-router.get('/property', propertyController.getAllProperties);
-router.get('/property/:id', propertyController.getProperty);
-
+router.post("/property", propertyController.addProperty);
+router.get("/property", propertyController.getAllProperties);
+router.get("/property/:id", propertyController.getProperty);
+router.get('/property/update/:id', propertyController.editProperty);
 module.exports = router;
