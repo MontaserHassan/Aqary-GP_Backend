95da920ff5edb954ad00de00b216f3c4554a48f7
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2023-06-03 20:29:11 +0300

    Get all Properties eidit and Update

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,11 +1,11 @@
 const Property = require("../models/propertyModel");
 const { asyncFunction } = require("../middlewares/asyncHandler");
-const { createUrlProperty, deleteUrlPhoto } = require("../middlewares/fileParser");
-
-
+const {
+  createUrlProperty,
+  deleteUrlPhoto,
+} = require("../middlewares/fileParser");
 
 //////////////////////////////////// handle endTime ///////////////////////////////////////
-
 
 const calculateEndTime = (duration) => {
   const millisecondsInDay = 24 * 60 * 60 * 1000; // Assuming a day is 24 hours
@@ -24,15 +24,16 @@ const calculateEndTime = (duration) => {
   return endTime;
 };
 
-
 //////////////////////////////////// create property ///////////////////////////////////////
 
-
 const createProperty = asyncFunction(async (req, res) => {
-  if (!req.files || req.files.length === 0) throw { status: 400, message: "no images uploaded" };
-    const photos = await Promise.all(
+  if (!req.files || req.files.length === 0)
+    throw { status: 400, message: "no images uploaded" };
+  const photos = await Promise.all(
     req.files.map(async (file) => {
-      const photo = await createUrlProperty( `${file.destination}/${file.filename}` );
+      const photo = await createUrlProperty(
+        `${file.destination}/${file.filename}`
+      );
       return photo;
     })
   );
@@ -52,86 +53,69 @@ const createProperty = asyncFunction(async (req, res) => {
     subscribe: req.body.subscribe,
     endTime: calculateEndTime(req.body.subscribe),
   });
-  property.save()
-  .then(() => res.status(200).send(property))
-  .catch((error) => res.json(error));
+  property
+    .save()
+    .then(() => res.status(200).send(property))
+    .catch((error) => res.json(error));
 });
 
-// get all Properties
-const getAllProperties = (req, res, next) => {
-  Property.find()
-    .then((response) => {
-      res.json(response);
-    })
-    .catch((error) => {
-      res.json(error);
-    });
-};
+//////////////////////////////// get all Properties//////////////////////////////////////
 
-const getProperty = (req, res, next) => {
+const getAllProperties = asyncFunction(async (req, res) => {
+  const properties = await Property.find();
+  if (!properties) throw { status: 404, message: "No Properties Found" };
+  res.status(200).send(properties);
+});
+
+//////////////////////////////////////////////get Property by id /////////////////////
+
+const getProperty = asyncFunction(async (req, res) => {
   const {
     params: { id },
   } = req;
-
-  Property.findById(id)
-    .then((response) => {
-      res.json(response);
-    })
-    .catch((error) => {
-      res.json(error);
-    });
-};
+  const property = await Property.findById(id);
+  if (!property) throw { status: 404, message: "No Properties Found" };
+  res.status(200).send(property);
+});
 
 // edit Property by id
-const editProperty = (req, res, next) => {
+
+
+const editProperty = asyncFunction(async (req, res) => {
   const {
     params: { id },
   } = req;
-  const {
-    address: address,
-    city: city,
-    title: title,
-    level: level,
-    rooms: rooms,
-    baths: baths,
-    area: area,
-    description: description,
-    price: price,
-    contractPhone: contractPhone,
-    photos: photos,
-    paymentOption: paymentOption,
-    subscribe: subscribe,
-  } = req.body;
-  Property.findById(id)
-    .then((property) => {
-      property.address = address;
-      property.city = city;
-      property.level = level;
-      property.rooms = rooms;
-      property.baths = baths;
-      property.area = area;
-      property.description = description;
-      property.contractPhone = contractPhone;
-      property.photos = photos;
-      property.paymentOption = paymentOption;
-      property.subscribe = subscribe;
 
-      return property.save();
-    })
-    .then(() => {
-      res.json("Updated Succesful");
-    })
-    .catch((error) => {
-      res.json(error);
-    });
-};
+  const selectedProperty = await Property.findById(id);
+  if (! selectedProperty) throw {status: 404, message: 'property not found'};
+  
+  selectedProperty.address = req.body.address;
+  selectedProperty.city = req.body.city;
+  selectedProperty.level = req.body.level;
+  selectedProperty.rooms = req.body.rooms;
+  selectedProperty.baths = req.body.baths;
+  selectedProperty.area = req.body.area;
+  selectedProperty.description = req.body.description;
+  selectedProperty.price = req.body.price;
+  selectedProperty.contractPhone = req.body.contractPhone;
+  selectedProperty.photos = req.body.photos;
+  selectedProperty.paymentOption = req.body.paymentOption;
+  selectedProperty.subscribe = req.body.subscribe;
+  selectedProperty.endTime = req.body.endTime;
+  const updatedProperty = await selectedProperty.save();
+
+  if(!updatedProperty) throw {status: 404, message: 'property not found'};
+  
+  res.status(200).send(updatedProperty);
+  
+});
 
 const deleteProperty = (req, res, next) => {
   const {
     params: { id },
   } = req;
   Property.findByIdAndDelete(id)
-    .then(() => res.json("Deleted Success") )
+    .then(() => res.json("Deleted Success"))
     .catch((error) => res.json(error));
 };
 
