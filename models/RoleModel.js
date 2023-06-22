acf86c0507efdc41d10fa7eb1c1c1179db0582d9
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-06-22 18:02:03 +0300

    edit in permission and rolling & edit in role schema model

--- models/RoleModel.js
+++ models/RoleModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const defaultRoles = require('./defaults/roles');
 
 const actionSchema = new mongoose.Schema({
-  rolePage: { type: Boolean, default: false },
+  // 1 is most powerfull value
+  // 1 can do anything can 2 or 3 can it
+  // 0 blocked User
+  roleRank: { tpye: Number, default: 0, unique: true },
   canBlockUser: { type: Boolean, default: false },
   canDeletePropertyAds: { type: Boolean, default: false },
   canDeleteProperty: { type: Boolean, default: false },
@@ -19,5 +23,12 @@ const roleSchema = new mongoose.Schema({
 });
 
 const RoleModel = mongoose.model('Role', roleSchema);
+
+RoleModel.collection.once('createIndex', async () => {
+  const rolesCount = await RoleModel.countDocuments();
+  if (rolesCount === 0) {
+    await RoleModel.insertMany(defaultRoles);
+  }
+});
 
 module.exports = RoleModel;
