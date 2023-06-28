c42c3c7231ad40ed2303004fbbed85d4d47cd31f
Omar <mromarhesham1999@gmail.com>
2023-06-28 21:08:09 +0300

    User Model

--- routes/customer/authRoutes.js
+++ routes/customer/authRoutes.js
@@ -1,13 +1,14 @@
 /* eslint-disable eol-last */
 /* eslint-disable no-multiple-empty-lines */
 const express = require('express');
+const authController = require('./../controllers/authController');
 
 
 const router = express.Router();
 
 
 router.post('/login');
-router.post('/sing-up');
+router.post('/singup', authController.signup);
 
 
 module.exports = router;
