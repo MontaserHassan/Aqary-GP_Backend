a0879dbb037aff32e31108b22e8e01af68792e7f
Omar <mromarhesham1999@gmail.com>
2023-06-29 20:08:54 +0300

    Modifing req.body in the authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,12 @@ const User = require('./../models/userModel');
 const { asyncFunction }= require('./../middlewares/asyncHandler');
 
 exports.signup = asyncFunction(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
 
     res.status(201).json({
         status: 'success',
