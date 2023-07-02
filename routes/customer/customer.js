5410864cf5b6355890e1723b13046739f77eeddd
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-07-02 19:52:13 +0200

    solve conflict

--- routes/customer/customer.js
+++ routes/customer/customer.js
@@ -4,11 +4,11 @@ const authRoutes = require('./authRoute');
 const propertyRoutes = require('./propertyRoutes');
 const userRoutes = require("./userPropertyRoutes");
 const checkoutRoutes = require('./checkout');
-<<<<<<< HEAD
 const transactionRoutes = require('./TransactionRoutes')
 const cityRoutes = require('./cityRoute')
 const havePermission = require('../../middlewares/havePermission');
 const roleName = require('../../middlewares/roleName');
+const authController = require('./../../controllers/authController');
 // const authRoute = require('./authRoute');
 const router = express.Router();
 
@@ -20,14 +20,6 @@ router.use('/city', cityRoutes); // ---> route to property
 
 router.use('/transaction', transactionRoutes); // ---> route to
 // middleware for check current user
-=======
-const transactionRoutes = require('./TransactionRoutes');
-const authController = require('./../../controllers/authController');
-
-
-const router = express.Router();
-
->>>>>>> a5631318c4014db333765f3b31d0b2894c60a38f
 
 router.use('/auth', authRoutes);
 router.use('/property', propertyRoutes);
