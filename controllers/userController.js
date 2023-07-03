289f38e657ae6e34cae06cf53e6d5cf85e437ce2
Omar <mromarhesham1999@gmail.com>
2023-07-04 00:33:57 +0300

    Finshing udateUserInfo function

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,15 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const { asyncFunction } = require('../middlewares/asyncHandler');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
 
@@ -18,8 +27,20 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   exports.updateUserInfo = asyncFunction(async (req, res, next) => {
     //1) Create error if user posts password data
       if(req.body.password || req.body.passwordConfirm) throw{ status: 400, message: 'This route is not for password update!'};
-    //2) Update user document
+
+    //2) Filtered out unwanted fields  
+    const filterBody = filterObj(req.body, 'firstName', 'lastName', 'birthdate');
+
+    //3) Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+      new: true,
+      runValidators: true
+    });
+
     res.status(200).json({
-      status: 'success'
+      status: 'success',
+      data: {
+        user: updatedUser
+      }
     })
   }) 
