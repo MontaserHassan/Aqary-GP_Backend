55e4b32b1ba37111c52abe2bdf25cb3ea99921e4
Omar <mromarhesham1999@gmail.com>
2023-07-04 00:09:07 +0300

    Adding updateUserInfo route

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,8 +15,11 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     });
   });
 
-  exports.updateUser = asyncFunction(async (req, res, next) => {
+  exports.updateUserInfo = asyncFunction(async (req, res, next) => {
     //1) Create error if user posts password data
       if(req.body.password || req.body.passwordConfirm) throw{ status: 400, message: 'This route is not for password update!'};
     //2) Update user document
+    res.status(200).json({
+      status: 'success'
+    })
   }) 
